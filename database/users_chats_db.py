70c7118d0f49541f70d748ffeb1074337371e6e3
uk365 <126067835+uk365@users.noreply.github.com>
2023-09-18 02:09:44 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -6,8 +6,8 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.Users
-        self.grp = self.db.Groups
+        self.col = self.db.users
+        self.grp = self.db.groups
 
 
     def new_user(self, id, name):
