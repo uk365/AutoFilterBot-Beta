8e2ea511b5ff21fa0bf9050602016c20bf3795ca
uk365 <126067835+uk365@users.noreply.github.com>
2023-09-22 20:59:27 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -72,12 +72,12 @@ async def answer(bot, query):
         if string:
             switch_pm_text += f" for {string}"
         try:
-            await query.answer(results=results,
+            await query.answer(#results=results,
                            is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="help",
-                          # next_offset=str(next_offset)
+                           next_offset=str(next_offset)
                               )
         except QueryIdInvalid:
             pass
