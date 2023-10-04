74c3c29a397b40a0471639fb68471f80096856ab
uk365 <126067835+uk365@users.noreply.github.com>
2023-10-05 01:51:09 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -9,10 +9,10 @@ async def update_users_data():
         user_id = int(user['id'])
         try:
             default = {
-                'is_verified'=False,
-                'verified_time'="",
-                'verify_token'="",
-                'link'=""
+                'is_verified'='False',
+                'verified_time'='',
+                'verify_token'='',
+                'link'=''
             }
             await db.update_x(user_id, default) 
         except Exception as e:
